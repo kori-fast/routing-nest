2fb63aeff50f0408a794c3f4954c80b02420bf6e
Kori Fast <kori.fast.c@gmail.com>
2026-02-09 19:00:42 +0100

    Added catch-all archive page

--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from '@/components/news-list'
-import { getNewsForYear } from '@/lib/news'
-
-export default function FilteredNewsPage({ params }) {
-	const news = getNewsForYear(params.year)
-
-	return <NewsList news={news} />
-}
