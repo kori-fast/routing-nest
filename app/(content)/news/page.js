38f5f35d2c0df7293ba9da7e275198e3396b5e48
Kori Fast <kori.fast.c@gmail.com>
2026-02-13 19:55:22 +0100

    Unefficient client data fetching example

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,13 +1,53 @@
-import { DUMMY_NEWS } from '@/dummy-news'
+'use client'
+
 import NewsList from '@/components/news-list'
+import { useEffect, useState } from 'react'
 
 export default function NewsPage() {
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
+	const [news, setNews] = useState([])
+
+	useEffect(() => {
+		async function fetchNews() {
+			setIsLoading(true)
+			const response = await fetch('http://localhost:8080/news')
+
+			if (!response.ok) {
+				setIsLoading(false)
+				setError('Failed to fetch news')
+			}
+
+			setIsLoading(false)
+
+			const news = await response.json()
+
+			setNews(news)
+		}
+
+		fetchNews()
+	}, [])
+
+	if (isLoading) {
+		return <p>Loading...</p>
+	}
+
+	if (error) {
+		return <p>{error}</p>
+	}
+
+	let newsContent
+
+	if (news.length) {
+		newsContent = <NewsList news={news} />
+	}
+
 	return (
 		<>
 			<header>
 				<h1>News Page</h1>
 			</header>
-			<NewsList news={DUMMY_NEWS} />
+			{newsContent}
 		</>
 	)
 }
